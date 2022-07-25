2e28108a4c2f8d7505063e76d000944766b20080
Dave <dave@spudooli.com>
2022-07-25 18:22:31 +1200

    DRY lastid, add right sidebar

--- news/main.py
+++ news/main.py
@@ -1,22 +1,19 @@
 from news import app, db
 from flask import render_template, redirect
-import redis
 from datetime import date
 from datetime import datetime, timedelta
 
 
-r = redis.StrictRedis('localhost', 6379, charset="utf-8", decode_responses=True)
-
 def getthenewsagain(section):
     
-    if section == "business":
+    if section == "business" or section == "technology":
         newsday = datetime.today() - timedelta(days=1)
         newsday = f'{newsday.strftime("%Y-%m-%d")} 00:00:00'
     else:
         today = date.today()
         newsday = f'{today.strftime("%Y-%m-%d")} 00:00:00'
     cursor = db.mysql.connection.cursor()
-    cursor.execute("SELECT id, headline, summary, source, url, clusterid, section, scrapedate, pubdate, featured FROM news where clusterid IS NOT NULL and section LIKE %s and scrapedate > %s order by featured", (section, newsday))
+    cursor.execute("SELECT id, headline, summary, source, url, clusterid, section, scrapedate, pubdate, featured FROM news where clusterid IS NOT NULL and section LIKE %s and scrapedate > %s", (section, newsday))
     newsitems = cursor.fetchall()
     desc = cursor.description
     column_names = [col[0] for col in desc]
@@ -24,6 +21,15 @@ def getthenewsagain(section):
         for row in newsitems]
     cursor.close()
     return data
+
+
+def getthelastid():
+    # Get the last id out of the database to provide a way to warn there is new content
+    cursor = db.mysql.connection.cursor()
+    cursor.execute("SELECT id FROM news order by id desc limit 1")  
+    lastid = cursor.fetchone()
+    cursor.close()
+    return lastid[0]
 
 @app.route('/', strict_slashes=False, defaults={'section': None} )
 @app.route("/<section>")
@@ -59,14 +65,10 @@ def index(section):
     else:
         return redirect('/', code=301)
 
-    # Get the last id out of the database to provide a way to warn there is new content
-    cursor = db.mysql.connection.cursor()
-    cursor.execute("SELECT id FROM news order by id desc limit 1")  
-    lastid = cursor.fetchone()
-    cursor.close()
-
+    lastid = getthelastid()
 
     return render_template('index.html', item1 = item1, section = section, lastid = lastid)
+
 
 @app.route("/liveupdates/<int:lastid>")
 def liveupdates(lastid):
@@ -80,7 +82,6 @@ def liveupdates(lastid):
     else:
         print("It's not bigger")
         return ""
-
 
 
 @app.errorhandler(404)
