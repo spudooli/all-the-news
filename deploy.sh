f0c215bb8f124e1dbcd62fe7295409b87a6c5d02
Dave <dave@spudooli.com>
2022-08-16 17:34:13 +1200

    Formatting just becasue

--- deploy.sh
+++ deploy.sh
@@ -7,16 +7,16 @@ fi
 
 echo "Copying the app..."
 
-cp -p /home/dave/Sites/all-the-news/news/* /var/www/news/news/
-cp -pr /home/dave/Sites/all-the-news/scrapers/* /var/www/news/scrapers/
+cp -p /home/dave/Sites/news/news/* /var/www/news/news/
+cp -pr /home/dave/Sites/news/scrapers/* /var/www/news/scrapers/
 
 echo "Deploying the static assets..."
 
-cp -pr /home/dave/Sites/all-the-news/news/static/* /var/www/news/news/static/
-cp -p /home/dave/Sites/all-the-news/news/config.py /var/www/news/news/config.py
+cp -pr /home/dave/Sites/news/news/static/* /var/www/news/news/static/
+cp -p /home/dave/Sites/news/news/config.py /var/www/news/news/config.py
 
 echo "Deploying the templates..."
-cp -rp /home/dave/Sites/all-the-news/news/templates/* /var/www/news/news/templates/
+cp -rp /home/dave/Sites/news/news/templates/* /var/www/news/news/templates/
 
 echo "Clearing the production cache..."
 rm -rf /var/www/news/news/__pycache__
--- news/main.py
+++ news/main.py
@@ -22,8 +22,8 @@ def getthenewsagain(section):
     newsitems = cursor.fetchall()
     desc = cursor.description
     column_names = [col[0] for col in desc]
-    data = [dict(zip(column_names, row))  
-        for row in newsitems]
+    data = [dict(zip(column_names, row))
+            for row in newsitems]
     cursor.close()
     return data
 
@@ -36,8 +36,8 @@ def getthetrendingitems(keyword):
     newsitems = cursor.fetchall()
     desc = cursor.description
     column_names = [col[0] for col in desc]
-    data = [dict(zip(column_names, row))  
-        for row in newsitems]
+    data = [dict(zip(column_names, row))
+            for row in newsitems]
     cursor.close()
     return data
 
@@ -45,21 +45,21 @@ def getthetrendingitems(keyword):
 def getthelastid():
     # Get the last id out of the database to provide a way to warn there is new content
     cursor = db.mysql.connection.cursor()
-    cursor.execute("SELECT id FROM news order by id desc limit 1")  
+    cursor.execute("SELECT id FROM news order by id desc limit 1")
     lastid = cursor.fetchone()
     cursor.close()
     return lastid[0]
 
 
-def getfeatured() :
+def getfeatured():
     # Get the featured/trending news items
     cursor = db.mysql.connection.cursor()
     cursor.execute("SELECT id, title, url, category FROM featured_news")
     featureditems = cursor.fetchall()
     desc = cursor.description
     column_names = [col[0] for col in desc]
-    featured = [dict(zip(column_names, row))  
-        for row in featureditems]
+    featured = [dict(zip(column_names, row))
+                for row in featureditems]
     cursor.close()
     return featured
 
@@ -67,81 +67,66 @@ def getfeatured() :
 def lastupdated():
     # Get the last updated date
     cursor = db.mysql.connection.cursor()
-    cursor.execute("SELECT scrapedate FROM news order by scrapedate desc limit 1")
+    cursor.execute(
+        "SELECT scrapedate FROM news order by scrapedate desc limit 1")
     lastupdated = cursor.fetchone()
     cursor.close()
     return lastupdated[0]
 
 
-@app.route('/', strict_slashes=False, defaults={'section': None} )
+@app.route('/', strict_slashes=False, defaults={'section': None})
 @app.route("/<section>")
 def index(section):
     if section == 'business':
         section = 'Business'
         item1 = getthenewsagain("business")
-
     elif section == 'technology':
         section = 'Technology'
         item1 = getthenewsagain("technology")
-    
     elif section == 'world':
         section = 'World'
         item1 = getthenewsagain("world")
-    
     elif section == 'sport':
         section = 'Sport'
         item1 = getthenewsagain("sport")
-
     elif section == 'politics':
         section = 'Politics'
         item1 = getthenewsagain("politics")
-    
     elif section is None:
         section = 'NZ News'
         item1 = getthenewsagain("nz")
-
     elif section == 'te-ao-maori':
         section = 'Te Ao Māori'
         item1 = getthenewsagain("te ao maori")
-
     else:
         return redirect('/', code=301)
 
     lastid = getthelastid()
-
     featured = getfeatured()
-
     lastupdateddate = lastupdated()
-
-    return render_template('index.html', item1 = item1, section = section, lastid = lastid, featured = featured, lastupdateddate = lastupdateddate)
+    return render_template('index.html', item1=item1, section=section, lastid=lastid, featured=featured, lastupdateddate=lastupdateddate)
 
 
 @app.route("/trending/<url>")
 def trending(url):
-
-    print(url)
-
     cursor = db.mysql.connection.cursor()
-    cursor.execute("SELECT id, keywords, title, url FROM featured_news where url LIKE %s", ("%" + url + "%", ))  
+    cursor.execute(
+        "SELECT id, keywords, title, url FROM featured_news where url LIKE %s", ("%" + url + "%", ))
     data = cursor.fetchone()
-
     keyword = data[1]
     section = data[2]
     item1 = getthetrendingitems(keyword)
-
     lastid = getthelastid()
-
     featured = getfeatured()
-
     lastupdateddate = lastupdated()
 
-    return render_template('index.html', item1 = item1, section = section, lastid = lastid, featured = featured, lastupdateddate = lastupdateddate)
+    return render_template('index.html', item1=item1, section=section, lastid=lastid, featured=featured, lastupdateddate=lastupdateddate)
 
 
 @app.route("/liveupdates/<int:lastid>")
 def liveupdates(lastid):
     cursor = db.mysql.connection.cursor()
-    cursor.execute("SELECT id FROM news order by id desc limit 1")  
+    cursor.execute("SELECT id FROM news order by id desc limit 1")
     currentlastid = cursor.fetchone()
     cursor.close()
     if int(currentlastid[0]) > lastid:
@@ -159,6 +144,6 @@ def page_not_found(e):
 
 @app.errorhandler(500)
 def internal_error(e):
-    #logging.exception(e)
+    # logging.exception(e)
     app.logger.exception(e)
     return render_template('404.html'), 500
